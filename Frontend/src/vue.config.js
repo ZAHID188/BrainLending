3e40642cad981b9bbc15d812223d7ecba4301fe6
zahid1888 <zahidpranta5@gmail.com>
2024-11-26 09:56:46 +0800

    deploy

--- Frontend/src/vue.config.js
+++ Frontend/src/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   outputDir: '../../../deploying_International_compettion/Elearning/docs'
+// })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/BrainLending/'  // Use 'BrainLending' as your repository name
+    : '/'
+}
